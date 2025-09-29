9c97402343b23e84028c7b88599efad8e8b15e31
ernikko <egorkuydin@gmail.com>
2025-09-30 00:17:38 +0800

    Create main.js

---
+++ frontend/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("testBtn");
+  btn.addEventListener("click", () => {
+    alert("Кнопка работает!");
+  });
+});
